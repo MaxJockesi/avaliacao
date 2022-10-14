da55fea629b40c700c5a8efad0dca50f8745720d
Fernando de Magalhães Toledo <114614766+FernandoDeMagalhaes@users.noreply.github.com>
2022-10-14 08:21:29 -0300

    contrato

---
+++ Contrato.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+def montar_calcular(massa ,molho ,queijo ,cobertura):
+    """
+    Recebe os ingredientes em string printa e retorna a pizza montada e seu valor de custo
+    
+    Lança exeção caso um dos valores não existam na base de dados
+
+    :param massa: str
+    :param molho: str
+    :param queijo: str
+    :param cobertura: str
+    
+    :return list pizza, float preco:
+
+    """
+    pass
+
+def cadastrar(tipo, nome, preço):
+    """
+    Recebe as informações de um ingrediente e cadastra no banco de dados
+
+    :param tipo: str
+    :param nome: str
+    :param preço: float
+    
+    """
+    pass
+
+def remover(nome):
+    """
+    Remove um ingrediente da base de dados
+    
+    Levanta exeção caso não exista tal ingrediente na base de dados
+
+    :param nome: str
+    
+    """
+    pass
+
+def listar():
+    """
+    Retorna a base de dados
+
+    :return dict dados:
+    
+    """
+    pass
+    
